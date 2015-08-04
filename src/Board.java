de280fb8c02b906e620abb9a9499f64c773b0fbd
Xin (Chris) Zou <x9zou@uwaterloo.ca>
2015-08-03 21:29:48 -0400

    Update Board.java

--- src/Board.java
+++ src/Board.java
@@ -1,10 +1,7 @@
 
 public class Board{
-
-	
 	public static void printout(int[][] board, int row, int column){
 		
-	
 	System.out.println("-------");
 	for(int i = 0; i < row; i++) {
 		for(int j = 0; j < column; j++) {
